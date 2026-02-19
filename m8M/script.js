762dc3c7841e3f537233f9a2889ee6672250f36b
Maxim <maximk0703@gmail.com>
2026-02-19 22:48:32 +0300

    Add files via upload

---
+++ m8M/script.js
@@ -0,0 +1,8 @@
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 7000);
