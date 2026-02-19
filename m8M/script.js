a0b3f19437bef3d9b5870f92f18447bcca147c75
Maxim <maximk0703@gmail.com>
2026-02-19 22:56:30 +0300

    Delete m8M directory

--- m8M/script.js
+++
@@ -1,8 +0,0 @@
-const slides = document.querySelectorAll('.slide');
-let index = 0;
-
-setInterval(() => {
-  slides[index].classList.remove('active');
-  index = (index + 1) % slides.length;
-  slides[index].classList.add('active');
-}, 7000);
